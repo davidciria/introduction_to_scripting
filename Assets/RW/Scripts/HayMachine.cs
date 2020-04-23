428e0272a01bef46a1cb333d9781e2277298a8ea
David Ciria <david.ciria01@estudiant.upf.edu>
2020-04-23 12:40:22 +0200

    Init config after lab2 session

---
+++ Assets/RW/Scripts/HayMachine.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HayMachine : MonoBehaviour
+{
+
+    public float movementSpeed;
+
+    public float horizontalBoundary = 22;
+
+    public GameObject hayBalePrefab; //Reference to the Hay Bale prefab.
+    public Transform haySpawnpoint; //The point from which the hay will to be shot.
+    public float shootInterval; //The smallest amount of time between shots
+    private float shootTimer; //A timer that to keep track whether the machine can shoot
+
+    private void UpdateMovement()
+    {
+
+        Debug.Log(transform.position.x);
+        float horizontalInput = Input.GetAxisRaw("Horizontal"); // 1
+        if (horizontalInput < 0 && transform.position.x > -horizontalBoundary) // 1
+        {
+            transform.Translate(transform.right * -movementSpeed * Time.deltaTime);
+        }
+        else if (horizontalInput > 0 && transform.position.x < horizontalBoundary) // 2
+        {
+            transform.Translate(transform.right * movementSpeed * Time.deltaTime);
+        }
+
+        UpdateShooting();
+    }
+
+    private void ShootHay()
+    {
+        Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+    }
+
+    private void UpdateShooting()
+    {
+        shootTimer -= Time.deltaTime;
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space))
+        {
+            shootTimer = shootInterval;
+            ShootHay();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+    }
+}
---
+++ Assets/RW/Scripts/Sheep.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    public float runSpeed;
+    public float gotHayDestroyDelay;
+    private bool hitByHay;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) // 1
+    {
+        if (other.CompareTag("Hay") && !hitByHay) // 2
+        {
+            Destroy(other.gameObject); // 3
+            HitByHay(); // 4
+        }
+    }
+
+    private void HitByHay()
+    {
+        hitByHay = true; // 1
+        runSpeed = 0; // 2
+        Destroy(gameObject, gotHayDestroyDelay); // 3
+    }
+
+}
